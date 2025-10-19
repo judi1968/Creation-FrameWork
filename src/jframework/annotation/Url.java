27342ebe716eb23c49f75d2a6c05bf9c25fa7569
Judi <127020548+judi1968@users.noreply.github.com>
2025-10-19 13:10:34 +0300

    Create Url.java

---
+++ src/jframework/annotation/Url.java
@@ -0,0 +1,18 @@
+package jframework.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method; 
+
+// =================== ANNOTATIONS ===================
+
+// Annotation pour la table
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Url {
+    String description();
+}
+
